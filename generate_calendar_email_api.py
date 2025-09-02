03e127ab534043f1050e26faea4be30d8c38f79a
Georgetown Day School <tlyons@gds.org>
2025-09-01 22:51:40 -0400

    email to TL

--- generate_calendar_email_api.py
+++ generate_calendar_email_api.py
@@ -28,9 +28,7 @@ OUTPUT_HTML_FILE = "facilities_security_email.html"
 FROM_EMAIL = os.environ.get('FROM_EMAIL', 'gds-events-notifications@gds.org')
 FROM_PASSWORD = os.environ.get('GMAIL_APP_PASSWORD')
 TO_EMAILS = [
-    'nmarkley@gds.org',        # Natalie
-    'kdaniels@gds.org',        # Khalid
-    'shharris@gds.org'         # Shelley
+    'tlyons@gds.org',        # Natalie
 ]
 CC_EMAILS = [
     'tlyons@gds.org'  # You for monitoring
